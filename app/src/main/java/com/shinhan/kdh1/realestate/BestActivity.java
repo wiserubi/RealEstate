da0161fe3744db3a32ea67d4557105359b9ce00a
wiserubi <wiserubi@gmail.com>
2017-09-15 16:18:11 +0900

    메인액티비티 3레이아웃으로 구성

---
+++ app/src/main/java/com/shinhan/kdh1/realestate/BestActivity.java
@@ -0,0 +1,13 @@
+package com.shinhan.kdh1.realestate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_best);
+    }
+}
