6fc86dba4e3778874fc63e4a298ef4f6e8e5958a
wiserubi <wiserubi@gmail.com>
2017-09-22 17:20:56 +0900

    4번째 레이아웃추가

--- app/src/main/java/com/shinhan/kdh1/realestate/MainActivity.java
+++ app/src/main/java/com/shinhan/kdh1/realestate/MainActivity.java
@@ -1,13 +1,18 @@
 package com.shinhan.kdh1.realestate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
